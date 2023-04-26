8976b61b80f0007fb92a427f5edeb9443b35cf33
acheronii <Jrchurch02@gmail.com>
2023-04-26 12:32:59 -0400

    added the rest of the buttons

--- Social Media Visualization/src/prj5/GUIWindow.java
+++ Social Media Visualization/src/prj5/GUIWindow.java
@@ -31,10 +31,97 @@ public class GUIWindow {
         this.channelList = channelList;
         window = new Window();
 
+        // buttons for each month
+        Button jan = new Button();
+        jan.setTitle("January");
+        jan.onClick(this, "clickedMonth");
+        window.addButton(jan, WindowSide.SOUTH);
+        Button feb = new Button();
+        feb.setTitle("February");
+        feb.onClick(this, "clickedMonth");
+        window.addButton(feb, WindowSide.SOUTH);
+        Button mar = new Button();
+        mar.setTitle("March");
+        mar.onClick(this, "clickedMonth");
+        window.addButton(mar, WindowSide.SOUTH);
+        Button apr = new Button();
+        apr.setTitle("April");
+        apr.onClick(this, "clickedMonth");
+        window.addButton(apr, WindowSide.SOUTH);
+        Button may = new Button();
+        may.setTitle("May");
+        may.onClick(this, "clickedMonth");
+        window.addButton(may, WindowSide.SOUTH);
+        Button jun = new Button();
+        jun.setTitle("June");
+        jun.onClick(this, "clickedMonth");
+        window.addButton(jun, WindowSide.SOUTH);
+        Button jul = new Button();
+        jul.setTitle("July");
+        jul.onClick(this, "clickedMonth");
+        window.addButton(jul, WindowSide.SOUTH);
+        Button aug = new Button();
+        aug.setTitle("August");
+        aug.onClick(this, "clickedMonth");
+        window.addButton(aug, WindowSide.SOUTH);
+        Button sep = new Button();
+        sep.setTitle("September");
+        sep.onClick(this, "clickedMonth");
+        window.addButton(sep, WindowSide.SOUTH);
+        Button oct = new Button();
+        oct.setTitle("October");
+        oct.onClick(this, "clickedMonth");
+        window.addButton(oct, WindowSide.SOUTH);
+        Button nov = new Button();
+        nov.setTitle("November");
+        nov.onClick(this, "clickedMonth");
+        window.addButton(nov, WindowSide.SOUTH);
+        Button dec = new Button();
+        dec.setTitle("December");
+        dec.onClick(this, "clickedMonth");
+        window.addButton(dec, WindowSide.SOUTH);
+        Button q1 = new Button();
+        q1.setTitle("Quarter 1");
+        q1.onClick(this, "clickedMonth");
+        window.addButton(q1, WindowSide.SOUTH);
+        Button q2 = new Button();
+        q2.setTitle("Quarter 2");
+        q1.onClick(this, "clickedMonth");
+        window.addButton(q2, WindowSide.SOUTH);
+        Button q3 = new Button();
+        q3.setTitle("Quarter 3");
+        q3.onClick(this, "clickedMonth");
+        window.addButton(q3, WindowSide.SOUTH);
+        Button q4 = new Button();
+        q4.setTitle("Quarter 4");
+        q4.onClick(this, "clickedMonth");
+        window.addButton(q4, WindowSide.SOUTH);
+
+        // buttons for choosing the sort method
+        Button sortEngage = new Button();
+        sortEngage.setTitle("Sort By Engagement Rate");
+        sortEngage.onClick(this, "clickedSortByEngagement");
+        window.addButton(sortEngage, WindowSide.NORTH);
+        Button sortName = new Button();
+        sortName.setTitle("Sort By Channel Name");
+        sortName.onClick(this, "clickedSortByName");
+        window.addButton(sortName, WindowSide.NORTH);
+
+        // add quit button
         Button quit = new Button();
         quit.onClick(this, "clickedQuit");
+        quit.setTitle("Quit");
         window.addButton(quit, WindowSide.NORTH);
 
+        // buttons for choosing the type of engagment
+        Button chooseTrad = new Button();
+        chooseTrad.setTitle("Traditional Engagement Rate");
+        chooseTrad.onClick(this, "clickedTraditional");
+        window.addButton(chooseTrad, WindowSide.WEST);
+        Button chooseReach = new Button();
+        chooseReach.setTitle("Reach Engagement Rate");
+        chooseReach.onClick(this, "clickedReach");
+        window.addButton(chooseReach, WindowSide.WEST);
     }
 
 
