a70a8e54ccf890b0c430ce0b34896f4ba0796a42
acheronii <Jrchurch02@gmail.com>
2023-04-26 11:48:27 -0400

    updated all the button presses

--- Social Media Visualization/src/prj5/GUIWindow.java
+++ Social Media Visualization/src/prj5/GUIWindow.java
@@ -22,11 +22,12 @@ public class GUIWindow {
     private ChannelList channelList;
     private String selectedMonth;
     private String engagementType;
+    private String sortType;
 
     public GUIWindow(ChannelList channelList) {
         selectedMonth = "January";
         engagementType = "Traditional";
-
+        sortType = "Name";
         this.channelList = channelList;
         window = new Window();
 
@@ -45,16 +46,46 @@ public class GUIWindow {
     }
 
 
+    /**
+     * sets the sort type to by name and then calls sortByName to sort and
+     * update the GUI
+     * 
+     * @param button
+     *            the button pressed
+     */
     public void clickedSortByName(Button button) {
+        sortType = "Name";
+        sortByName();
+    }
+
+
+    /**
+     * sorts the list by name and then updates the GUI
+     */
+    private void sortByName() {
         CompareByName comp = new CompareByName();
         channelList.sort(comp);
         update();
     }
-    
+
+
+    /**
+     * sets the sort type to by engagement and then calls sortBYEngagement to
+     * sort and update the GUI
+     * 
+     * @param button
+     *            the button pressed
+     */
     public void clickedSortByEngagement(Button button) {
+        sortType = "Engagement";
         sortByEngagement();
     }
 
+
+    /**
+     * sorts the list by the current engagement type during the selected month
+     * and then updates the GUI
+     */
     private void sortByEngagement() {
         if (engagementType == "Traditional") {
             CompareByTraditional comp = new CompareByTraditional();
@@ -67,21 +98,48 @@ public class GUIWindow {
         update();
     }
 
+
+    /**
+     * sets the engagement type to be traditional and calls sortByEngagement to
+     * sort and update the GUI
+     * 
+     * @param button
+     *            the button pressed
+     */
     public void clickedTraditional(Button button) {
         engagementType = "Traditional";
         sortByEngagement();
     }
 
 
+    /**
+     * sets the engagement type to be reach and calls sortByEngagement to
+     * sort and update the GUI
+     * 
+     * @param button
+     *            the button pressed
+     */
     public void clickedReach(Button button) {
         engagementType = "Reach";
         sortByEngagement();
     }
 
 
+    /**
+     * sets the selected month based on which month button was pressed and then
+     * calls either sortByName or sortByEngagement to sort and update the GUI
+     * 
+     * @param button
+     *            the button pressed
+     */
     public void clickedMonth(Button button) {
-        //selectedMonth = ;
-        update();
+        selectedMonth = button.getTitle();
+        if (sortType == "Name") {
+            sortByName();
+        }
+        else {
+            sortByEngagement();
+        }
     }
 
 
@@ -90,7 +148,7 @@ public class GUIWindow {
      * choose what to display
      */
     private void update() {
-        
+
     }
 
 }
